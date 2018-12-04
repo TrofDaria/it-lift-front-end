63d8b7d3fd55219c8043b5694d4e61135fa4ce4f
dasha <trofimova.daria.d@gmail.com>
2018-12-04 20:11:41 +0600

    fix incorrect paths

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var concat = require('gulp-concat');
 
 gulp.task('concat_css', function () {
     gulp.src('src/styles/**/*.css')
-        .pipe(concat('all.css'))
-        .pipe(gulp.dest('build/css'))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('build/styles'))
 });
 gulp.task('replace_html', function () {
     gulp.src('src/templates/**/*.html')
-        .pipe(gulp.dest('build/templates'))
+        .pipe(gulp.dest('build'))
 });
 gulp.task('default', ['concat_css', 'replace_html']);
